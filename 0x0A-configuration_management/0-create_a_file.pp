668f6c1bb1b154de96a5447b27ee491b40c9685d
kim-thuita <kevinkoech265@gmail.com>
2023-08-26 04:44:45 -0700

    test

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-file {'tmp/school':
-  ensure   =>file,
-  path     =>'/tmp/school/',
-  mode     =>'0744',
-  owner    =>'www-data',
-  group    =>'www-data',
-  contains =>'I love Puppet'
+# Creates a file
+file { '/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
