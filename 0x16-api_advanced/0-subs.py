417d3bbbb81a6373bc0a099816164f3cd4ee4708
kim-thuita <kevinkoech265@gmail.com>
2023-10-11 03:49:37 +0300

    "Tetsing out nmber of subreddits"

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+"""The Subreddit taker"""
 import requests
 
 def number_of_subscribers(subreddit):
     """Query the api"""
-    if subreddit is none or not isinstance(subreddit, str):
+    if subreddit is None or not isinstance(subreddit, str):
         return 0
     else:
         user_agent = {'User-agent': 'Google Chrome Version 117.0.5938.134 '}
@@ -11,7 +12,13 @@ def number_of_subscribers(subreddit):
         response = requests.get(url, headers=user_agent)
         data = response.json()
 
-        return data.get('data').get('subscribers')
+        try:
+            return data.get('data').get('subscribers')
+        except Exception as e:
+            print(e)
+            return 0
+
+
 
 
 
