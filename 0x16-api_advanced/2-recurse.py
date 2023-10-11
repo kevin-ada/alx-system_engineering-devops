a0203d648156871a6ee6a0f32ec9b11fe17b2497
kim-thuita <kevinkoech265@gmail.com>
2023-10-11 04:10:10 +0300

    "Recurse it"

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+"""
+importing requests module
+"""
+
+from requests import get
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """
+    function that queries the Reddit API and returns a list containing the
+    titles of all hot articles for a given subreddit.
+    """
+
+    params = {'show': 'all'}
+
+    if subreddit is None or not isinstance(subreddit, str):
+        return None
+
+    user_agent = {'User-agent': 'Google Chrome Version 117.0.5938.134'}
+
+    url = 'https://www.reddit.com/r/{}/hot/.json?after={}'.format(subreddit,
+                                                                  after)
+
+    response = get(url, headers=user_agent, params=params)
+
+    if response.status_code == 200:
+        all_data = response.json()
+
+        try:
+            raw1 = all_data.get('data').get('children')
+            after = all_data.get('data').get('after')
+
+            if after is None:
+                return hot_list
+
+            for i in raw1:
+                hot_list.append(i.get('data').get('title'))
+
+            return recurse(subreddit, hot_list, after)
+        except Exception as e:
+            print(e)
+            print("None")
+        return
+
+    return None
